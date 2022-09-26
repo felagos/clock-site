cdf65baceb03dbfce0786e1bd89cce4d79b47486
Felipe Lagos <felagossanchez@gmail.com>
2022-09-26 20:33:55 -0300

    change var env name

--- src/environment.ts
+++ src/environment.ts
@@ -1,5 +1,5 @@
 export default {
-	URL: import.meta.env.VITE_URL,
+	URL_IMAGES: import.meta.env.VITE_URL_IMAGES,
 	ACCESS_KEY: import.meta.env.VITE_ACCESS_KEY,
 	QUOTE_API: import.meta.env.VITE_QUOTE_API
 };
--- src/services/image.service.ts
+++ src/services/image.service.ts
@@ -1,7 +1,7 @@
 import { ImageResponse } from "../models/image.model";
 import ENV from "../environment";
 
-const url = `${ENV.URL}/search/photos?query=forest&orientation=landscape`;
+const url = `${ENV.URL_IMAGES}/search/photos?query=forest&orientation=landscape`;
 
 const getRandom = (min: number, max: number) => Math.floor(Math.random() * (max - min + 1)) + min;
 
