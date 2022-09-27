d9ec2b7fe2a0616ccc812677d8cf5d6dacaa8625
Felipe Lagos <flagos@bancointernacional.cl>
2022-09-27 14:19:32 -0300

    function to period day

--- src/utils/clock.ts
+++ src/utils/clock.ts
@@ -6,6 +6,18 @@ export const getHour = () => {
 
 	const strHour = hour < 10 ? "0" + hour : hour;
 	const strMin = min < 10 ? "0" + min : min;
-	
+
 	return `${strHour}:${strMin}`;
-}
+};
+
+export const getPeriodDay = () => {
+	const time = new Date();
+	const hour = time.getHours();
+
+	if (hour < 12) return "Morning";
+	if (hour === 12) return "Noon";
+	if (hour <= 18) return "Afternnon";
+
+	return "Night";
+
+};
